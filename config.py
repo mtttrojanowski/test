44b2e0cf6a3b9e8dcdbcb561257516380be3744b
Anon PvP <bic191@live.com>
2021-10-04 04:31:37 -0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    NITRADO_TOKEN = "API_TOKEN"
-    DISCORD_TOKEN = "BOT TOKEN"
-    BOT_PREFIX = "!!"
-    EMBED_IMAGE = "IMAGE URL"
-    SERVERS = {
-        ServerID: Channel ID, # Server ID : Channel ID
-        9327465: 883133586251862036, # Server ID : Channel ID
-    }
